b88f7d9d5a9823a1e3734d85c3a7071c4e7194f9
راكينول إسلام <you@example.com>
2022-11-08 12:54:19 +0600

    sec comt

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import img from '../Home/3683230.jpg'
+
 
 const Home = () => {
     return (
-      <div>
-        <h2>Home</h2>
+      <div className="grid md:grid-cols-2 items-center">
+        <div>
+          <h2 className="text-3xl font-bold mb-2">❝ Delivery Service ❞</h2>
+          <p className="text-xl font-semibold">
+            "To earn the respect (and eventually love) of your customers, you
+            first have to respect those customers. That is why Golden Rule
+            behavior is embraced by most of the winning companies."
+          </p>
+        </div>
+        <div>
+          <img src={img} alt="" />
+        </div>
       </div>
     );
 };
