87c30356b0e14eb263945de1a22740a7c1034c37
راكينول إسلام <you@example.com>
2022-11-08 12:28:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Root from './components/Main/Root';
+import Services from './components/Services/Services';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root></Root>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+        {
+          path: "home",
+          element: <Home></Home>,
+        },
+        {
+          path: "services",
+          element: <Services></Services>
+        },
+      ],
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App md:mx-10">
+     <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9-7Jy86_3OnP64MsJkHDciYaT9CvI-yA",
+  authDomain: "assignment-eleventh.firebaseapp.com",
+  projectId: "assignment-eleventh",
+  storageBucket: "assignment-eleventh.appspot.com",
+  messagingSenderId: "978527258475",
+  appId: "1:978527258475:web:020ab258d0dcaea235ee2e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
