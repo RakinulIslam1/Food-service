cd0a36b8285c5537d0799bde5cef951fc1cdd017
راكينول إسلام <you@example.com>
2022-11-09 21:55:15 +0600

    env added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,13 +4,21 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC9-7Jy86_3OnP64MsJkHDciYaT9CvI-yA",
+//   authDomain: "assignment-eleventh.firebaseapp.com",
+//   projectId: "assignment-eleventh",
+//   storageBucket: "assignment-eleventh.appspot.com",
+//   messagingSenderId: "978527258475",
+//   appId: "1:978527258475:web:020ab258d0dcaea235ee2e",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyC9-7Jy86_3OnP64MsJkHDciYaT9CvI-yA",
-  authDomain: "assignment-eleventh.firebaseapp.com",
-  projectId: "assignment-eleventh",
-  storageBucket: "assignment-eleventh.appspot.com",
-  messagingSenderId: "978527258475",
-  appId: "1:978527258475:web:020ab258d0dcaea235ee2e",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
